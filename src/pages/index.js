1f8d04c5a6a97715086d316f924044f2ef7c8e55
El dano <danielpliego@gmail.com>
2019-11-21 08:29:02 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import { Container } from "../components/layoutComponents"
@@ -10,15 +10,18 @@ const IndexPage = () => (
   <Layout>
     <Container>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <IntroTextContainer>
+        <p>Hi I'm Dan Pliego, I live in Mexico City and <span>I do design, and front-end development since 2005.</span></p>
+        <p>You can picture myself as the link between your design and development team, pair me with an illustrator and a back-end developer and we can build almost anything, or hire me as a solo guy and I can help you research, plan, design, code and advertise your digital product and hopefully learn a lot from you and your team in the process.</p>
+        <p>Apart from work I love music, my cats, the sea, to play drums and for the past 3 months I traveled across europe to find inspiration and now i'm looking for new opportunities.</p>
+      </IntroTextContainer>
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
     </Container>
   </Layout>
 )
 
 export default IndexPage
+
+const IntroTextContainer = styled.div`
+  padding: 2rem;
+`
